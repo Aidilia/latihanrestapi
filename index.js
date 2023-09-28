2690654a1999bf08423f20ad070f49e7f9ee6351
Aidilia <fitriyantiaidilia@gmail.com>
2023-09-28 10:10:24 +0700

    setting routes ke server js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = 3000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var routes = require('./router');
+routes(app);
+
 app.listen(port, ()=>{
     console.log('Server is running on port ..');
 })
