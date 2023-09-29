32dfb531b860df2ea86128ced9b98470e1e4134c
Aidilia <fitriyantiaidilia@gmail.com>
2023-09-29 09:05:50 +0700

    verifikasi udpate

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
-function verifikasi(roles){
+function verifikasi(){
     return function(req, res, next){
+        var role = req.body.role;
         
         //cek token diheader
         var tokenWithBearer = req.headers.authorization;
         
         if(tokenWithBearer){
             var token = tokenWithBearer.split(' ')[1];
-
+ 
             //verifikasi
             jwt.verify(token, config.secret, function(error, decoded){
                 if(error){
                     return rest.status(401).send({auth:false, message:'Token tidak terdaftar'});
                 }else{
-                    if(roles==2){
+                    if(role == 2){
                         req.auth = decoded;
                         next();
                     }else{
